ee4741df57e50be10e65d0d0e56090dfcc501247
HabibaSabrina <habibasabrina92@gmail.com>
2023-06-10 01:58:48 +0600

    admin route is created

--- index.js
+++ index.js
@@ -43,6 +43,14 @@ const client = new MongoClient(uri, {
         const result = await usersCollection.insertOne(user);
         res.send(result);
     })
+    // admin user apis
+    app.get('/users/admin/:email'), async(req, res) =>{
+      const email = req.params.email;
+      const query = {email: email}
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' }
+      res.send(result);
+    }
     app.patch('/users/admin/:id', async (req, res) =>{
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)};
@@ -54,6 +62,7 @@ const client = new MongoClient(uri, {
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result)
     })
+    // instructor user apis
     app.patch('/users/instructor/:id', async (req, res) =>{
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)};
